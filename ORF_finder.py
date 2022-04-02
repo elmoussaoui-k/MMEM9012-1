fe1b0f0a9f2366dc68bf7edcc1e687efc1177238
elmoussaoui-k <99408035+elmoussaoui-k@users.noreply.github.com>
2022-04-02 02:43:47 +0200

    Finds an open reading frame

---
+++ ORF_finder.py
@@ -0,0 +1,53 @@
+# Import modules
+import re
+
+# Define functions
+def is_seq_valid(seq):
+    valid_bases = ['A', 'T', 'G', 'C']
+    for base in seq:
+        if base not in valid_bases:
+            return False
+    return True
+
+def open_reading_frame(seq):
+    ORF = max(re.findall(r'ATG(?:(?!TAA|TAG|TGA)...)*(?:TAA|TAG|TGA)', seq), key=len)
+    return(ORF)
+
+def dna_to_prot(seq):
+    table = {
+        'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+        'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+        'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+        'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+        'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+        'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+        'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+        'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+        'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
+    }
+    protein = ""
+    if len(seq) % 3 == 0:
+        for i in range(0, len(seq), 3):
+            codon = seq[i:i + 3]
+            protein += table[codon]
+    return protein.removesuffix("_")
+
+# Main function
+def main():
+    print('********************** ORF Finder Program **********************')
+    sequence = input("Please enter an oligonucleotide sequence : ").upper()
+    print('****************************************************************')
+    if (is_seq_valid(sequence)):
+        print('Original sequence: ' + sequence)
+        print('Open Reading Frame (DNA) : ' + open_reading_frame(sequence))
+        print('Open Reading Frame (AA) : ' + dna_to_prot(open_reading_frame(sequence)))
+    print('________________________________________________________________')
+
+main()

b41c0a03d7a3fdb023a94b331cab5460d706b6c8
elmoussaoui-k <99408035+elmoussaoui-k@users.noreply.github.com>
2022-05-11 17:13:10 +0200

    Update data_integrity_checker.py

--- data_integrity_checker.py
+++ data_integrity_checker.py
@@ -2,9 +2,9 @@
 print("")
 print('******************** DATA INTEGRITY CHECKER ********************')
 # Asks the user for the path to the file containing the original hashes :
-or_path = input("Please enter the path to the file containing the ORIGINAL hashes : ")
+or_path = input("Please enter the path to original_md5.txt : ")
 # Asks the user for the path to the file containing the recomputed hashes :
-re_path = input("Please enter the path to the file containing the RECOMPUTED hashes : ")
+re_path = input("Please enter the path to recomputed_md5.txt : ")
 print('----------------------------------------------------------------')
 # Opens the file containing the original hashes (received with the files from the GIGA genomics platform)
 original = open(or_path,"r").readlines()
@@ -18,17 +18,24 @@ for line in original:
     or_filename = line[34:78]
     # Update the previously created dictionary with those 2 information in the format key : value
     original_hashes_dict.update({or_filename:or_hash})
-
 # Opens the file containing the recomputed hashes (recalculated using md5 function within a terminal window)
 recomputed = open(re_path,"r").readlines()
 # Creates an empty dictionary that will store the keys & values that have been read from the previously opened file
 recomputed_hashes_dict = {}
 # For every line in the "recomputed hashes" file...
 for line in recomputed:
-    # Hash corresponds to index 0 to 32 of the line
-    re_filename = line[36:80]
-    # Filename corresponds to index 34 to 78 of the line
-    re_hash = line[93:125]
+    # Find the index of the pattern 'KE0' => store as start index for filename
+    filename_start_index = line.index("KE0")
+    # Find the index of the pattern '.' => store as end index for filename
+    filename_end_index = line.index(".")
+    # Find the index of the pattern '=' => store as start index for hash
+    hash_start_index = line.index("=")
+    # Add 2 to hash start index
+    hash_start_index = int(hash_start_index) +2
+    # Extracts hash based on previously determined indexes
+    re_filename = line[filename_start_index:filename_end_index]
+    # Extracts filename based on previously determined indexes (-1 = index of last character)
+    re_hash = line[hash_start_index:-1]
     # Update the previously created dictionary with those 2 information in the format key : value
     recomputed_hashes_dict.update({re_filename:re_hash})
 
@@ -39,5 +46,5 @@ if recomputed_hashes_dict != original_hashes_dict:
 # If they are perfect copies of each other, display another message
 else:
     print("")
-    print("The integrity of the data has been checked. All hashes match.")
+    print("It's ok : the integrity of the data has been checked. All hashes match.")
     print("")
